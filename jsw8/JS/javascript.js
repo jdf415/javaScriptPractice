85547af372737c08bc8bbb0140a5767a7f2eeb7a
Jesus D. Flores <floe415@gmail.com>
2019-10-31 14:21:29 -0700

    firstcommit
    adding button to change image

---
+++ jsw8/JS/javascript.js
@@ -0,0 +1,7 @@
+let picturesArray = ['imgs/plantOne.jpg', 'imgs/plantTwo.jpg']
+
+function myChange(){
+	// var cambiamos = document.getElementById("cambiado");
+	// console.log(hello)
+	var img = document.createElement('img')
+}
