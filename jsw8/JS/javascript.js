eaf778eafd48212fc1cc9bef02f27f7c5634e084
Jesus D. Flores <danyflores052@gmail.com>
2019-10-31 14:51:21 -0700

    secondcommit
    image displays and changes with button

--- jsw8/JS/javascript.js
+++ jsw8/JS/javascript.js
@@ -1,7 +1,22 @@
-let picturesArray = ['imgs/plantOne.jpg', 'imgs/plantTwo.jpg']
+let picturesArray = ['imgs/plantOne.jpg', 'imgs/plantTwo.jpg', 'imgs/plantThree.jpg', 'imgs/plantFour.jpg', 'imgs/greyMaxima.jpg', 'imgs/redToyota.jpg', 'imgs/hondaCivic.jpg']
+var index = 0;
 
+
+function buildImage(){
+	// variable to create an img element
+	var img = document.createElement('img');
+	img.src = picturesArray[index];
+	// append image to div with id
+	document.getElementById('content').appendChild(img);
+	// set height and width
+	img.width = '300';
+	img.height = '300';
+}
+//	CHANGE IMAGE FUNCTION
 function myChange(){
-	// var cambiamos = document.getElementById("cambiado");
-	// console.log(hello)
-	var img = document.createElement('img')
+// 	
+	var img = document.getElementById('content').getElementsByTagName('img')[0]
+	index++;
+	index = index % picturesArray.length; //this if for when its the last image to return to the 1st image
+	img.src = picturesArray[index];
 }
